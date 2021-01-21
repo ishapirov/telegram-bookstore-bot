d78f54d6491236d8c26d894cad08baeadd0e2a95
ishapirov <47495391+ishapirov@users.noreply.github.com>
2021-01-21 14:14:01 -0500

    removing tests before rewriting them

--- src/test/java/com/ishapirov/telegrambot/TelegramBotApplicationTests.java
+++ src/test/java/com/ishapirov/telegrambot/TelegramBotApplicationTests.java
@@ -1,21 +1,10 @@
 package com.ishapirov.telegrambot;
 
 import com.ishapirov.telegrambot.controller.WebHookController;
-import com.ishapirov.telegrambot.services.inputprocessing.UserCallbackRequest;
 import com.ishapirov.telegrambot.services.view.ViewService;
-import com.ishapirov.telegrambot.testdomain.CustomCallback;
-import com.ishapirov.telegrambot.testdomain.CustomMessage;
-import com.ishapirov.telegrambot.testdomain.CustomUser;
-import com.ishapirov.telegrambot.views.mainmenu.MainMenuView;
-import com.ishapirov.telegrambot.views.View;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
-import org.telegram.telegrambots.meta.api.objects.Message;
-import org.telegram.telegrambots.meta.api.objects.User;
-
-import java.util.ArrayList;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -31,53 +20,5 @@ class TelegramBotApplicationTests {
 	void contextLoads() {
 		assertThat(controller).isNotNull();
 	}
-
-	@Test
-	void testUserCallbackRequest() {
-		Message message = new CustomMessage(1);
-		User user = new CustomUser(123);
-		MainMenuView mainmenuview = viewService.getMainMenuView();
-		String data = UserCallbackRequest.generateQueryMessage(mainmenuview.getTypeString(),mainmenuview.currencyText());
-		CallbackQuery callback = new CustomCallback(data,user,message);
-		UserCallbackRequest userCallbackRequest = new UserCallbackRequest(callback);
-		assertThat(userCallbackRequest.getChatId() == 1);
-		assertThat(userCallbackRequest.getUserId() == 123);
-		assertThat(userCallbackRequest.getButtonClicked().equals("currency"));
-		assertThat(userCallbackRequest.getViewInWhichButtonWasClicked().equals("mainmenu"));
-	}
-
-	@Test
-	void testGetNextView() {
-		Message message = new CustomMessage(1);
-		User user = new CustomUser(123);
-		MainMenuView mainmenuview = viewService.getMainMenuView();
-		String data = UserCallbackRequest.generateQueryMessage(mainmenuview.getTypeString(),mainmenuview.currencyText());
-		CallbackQuery callback = new CustomCallback(data,user,message);
-		UserCallbackRequest userCallbackRequest = new UserCallbackRequest(callback);
-		assertThat(viewService.getView(userCallbackRequest.getViewInWhichButtonWasClicked()).equals(mainmenuview));
-		View nextView = mainmenuview.getNextView(userCallbackRequest.getButtonClicked(),userCallbackRequest);
-		assertThat(nextView.equals(viewService.getCurrencySelectionView()));
-	}
-
-	@Test
-	void testViewService() {
-		for(View view: new ArrayList<View>(viewService.getViewMap().values()))
-			assertThat(view != null);
-	}
-
-//	@Test
-//	void testUserCallbackRequestWithPage() {
-//		Message message = new CustomMessage(1);
-//		User user = new CustomUser(123);
-//		BookCatalogView bookCatalogView = viewService.getBookCatalogView();
-//		String data = UserCallbackRequest.generateQueryMessage(bookCatalogView.getTypeString(),bookCatalogView.currencyText())
-//		CallbackQuery callback = new CustomCallback("mainmenu,currency,0",user,message);
-//		UserCallbackRequest userCallbackRequest = new UserCallbackRequest(callback);
-//		assertThat(userCallbackRequest.getChatId() == 1);
-//		assertThat(userCallbackRequest.getUserId() == 123);
-//		assertThat(userCallbackRequest.getButtonClicked().equals("currency"));
-//		assertThat(userCallbackRequest.getViewInWhichButtonWasClicked().equals("mainmenu"));
-//		assertThat(userCallbackRequest.getPage().equals(0));
-//	}
 
 }
